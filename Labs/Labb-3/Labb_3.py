23c1fd720be5496a2d62cdf2b719e89593085245
SamiFatmi <89996292+SamiFatmi@users.noreply.github.com>
2021-10-02 23:19:12 +0200

    Not showing plot when errors

--- Labs/Labb-3/Labb_3.py
+++ Labs/Labb-3/Labb_3.py
@@ -3,7 +3,7 @@ from shapes import Circle
 from shapes import Rectangle
 import math 
 import matplotlib.pyplot as plt
-
+import time
 import os
 
 
@@ -11,25 +11,31 @@ circle_list=[]
 square_list=[]
 rectangle_list=[]
 
+choice1,choice2,choice3,choice4=0,0,0,0
 
-main_menu="1. Create a new shape\n2. Select a shape\n3. Quit\n"
 
-menu_1="What type of shape ?\nA.Circle\nB.Rectangle\nC.Square\nM. Go back to main menu\n"
-menu_circle="Please enter the x and y coordinates and the radius of your circle\n"
-menu_rectangle="Please enter the x and y coordinates and height and width of you rectangle\n"
-menu_square="Please enter the x and y coordinates and length of the side of your square \n"
+main_menu="\n1. Create a new shape\n2. Select a shape\n3. Quit\n\n\n"
 
-menu_2="What type of shape ?\nA.Circle\nB.Rectangle\nC.Square\nM. Go back to main menu\n"
-menu_select_circle="What do you want to do with this circle ?\n1. Area\n2. Circumference\n3. Move it\n4. Move it to an exact point\n5. Scale it\n6. Change radius \n7. Check if it contains a point\n\n8. Remove\n"
-menu_select_rectangle="What do you want to do with this rectangle ?\n1. Area\n2. Circumference\n3. Move it\n4. Move it to an exact point\n5. Scale it\n6. Change dimensions to exact values\n7. Rotate\n8. Make it horizontale\n9. Make it vertical\n10. Remove\n"
-menu_select_square="What do you want to do with this square ?\n1. Area\n2. Circumference\n3. Move it\n4. Move it to an exact point\n5. Scale it\n6. Change dimensions to exact values\n7. Rotate\n8. Make it horizontale\n9. Remove\n"
+menu_1="\nWhat type of shape ?\nA.Circle\nB.Rectangle\nC.Square\nM. Go back to main menu\n\n\n"
+menu_circle="Please enter the x and y coordinates and the radius of your circle\n\n\n"
+menu_rectangle="Please enter the x and y coordinates and height and width of you rectangle\n\n\n"
+menu_square="Please enter the x and y coordinates and length of the side of your square \n\n\n"
+
+menu_2="What type of shape ?\nA.Circle\nB.Rectangle\nC.Square\nM. Go back to main menu\n\n\n"
+menu_select_circle="What do you want to do with this circle ?\n1. Area\n2. Circumference\n3. Move it\n4. Move it to an exact point\n5. Scale it\n6. Change radius \n7. Check if it contains a point\n\n8. Remove\n\n\n"
+menu_select_rectangle="What do you want to do with this rectangle ?\n1. Area\n2. Circumference\n3. Move it\n4. Move it to an exact point\n5. Scale it\n6. Change dimensions to exact values\n7. Rotate\n8. Make it horizontale\n9. Make it vertical\n10. Remove\n\n\n"
+menu_select_square="What do you want to do with this square ?\n1. Area\n2. Circumference\n3. Move it\n4. Move it to an exact point\n5. Scale it\n6. Change dimensions to exact values\n7. Rotate\n8. Make it horizontale\n9. Remove\n\n\n"
 
 while True:
+
+    show_plot=True 
+
     os.system('cls' if os.name == 'nt' else 'clear')
     choice1=input(main_menu)
     if choice1 not in ["1","2","3"]:
         print("Please enter a valid choice")
-        continue
+        time.sleep(1.5)
+        show_plot=False 
 
     elif choice1=="1":
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -37,6 +43,8 @@ while True:
         if choice2!="A" and choice2!="B" and choice2!="C" and choice2!="M":
             os.system('cls' if os.name == 'nt' else 'clear')
             print("Invalid choice")
+            time.sleep(1.5)
+            show_plot=False 
         elif choice2 =="A":
             try:             
                 os.system('cls' if os.name == 'nt' else 'clear')
@@ -50,8 +58,12 @@ while True:
                     circle_list.append([name,Circle(x,y,radius)])
                 except (ValueError,TypeError) as err:
                     print(err)
+                    time.sleep(1.5)
+                    show_plot=False 
             except ValueError as err :
                 print(err)
+                time.sleep(1.5)
+                show_plot=False 
 
         elif choice2 =="B":
             try:
@@ -66,8 +78,12 @@ while True:
                     rectangle_list.append([name,Rectangle(x,y,side1,side2)])
                 except (ValueError,TypeError) as err:
                     print(err)
+                    time.sleep(1.5)
+                    show_plot=False 
             except (ValueError,TypeError) as err:
                 print(err)
+                time.sleep(1.5)
+                show_plot=False 
 
         elif choice2 =="C":
             try:
@@ -82,11 +98,16 @@ while True:
                     square_list.append([name,Rectangle(x,y,side,side)])
                 except (TypeError,ValueError) as err:
                     print(err)
+                    time.sleep(1.5)
+                    show_plot=False 
             except (ValueError,TypeError) as err:
                 print(err)
+                time.sleep(1.5)
+                show_plot=False 
 
 
         elif choice2 =="M":
+            show_plot=False 
             continue
     
 
@@ -96,13 +117,17 @@ while True:
         if choice2 not in ["A","B","C","M"]:
             os.system('cls' if os.name == 'nt' else 'clear')
             print("Please enter a valid choice")
+            time.sleep(1.5)
+            show_plot=False 
         elif choice2 =="A":
             if len(circle_list)==0:
                 os.system('cls' if os.name == 'nt' else 'clear')
                 print("There are no circles to be selected")
+                time.sleep(1.5)
+                show_plot=False 
             else:
                 os.system('cls' if os.name == 'nt' else 'clear')
-                print("Please select a circle by entering its number")
+                print("Please select a circle by entering its number\n\n")
                 for index_circle in range(len(circle_list)):
                     print(f"{index_circle}. {circle_list[index_circle][0]}")
                 choice3=input("")
@@ -110,33 +135,42 @@ while True:
 
                 if choice3>=len(circle_list) or choice3<0:
                     print("Invalid index")
+                    time.sleep(1.5)
+                    show_plot=False 
                 else:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     selected_shape=circle_list[choice3][1]
                     choice4=input(menu_select_circle)
                     if choice4 not in ["1","2","3","4","5","6","7"]:
                         print("Please enter a valid choice")
+                        time.sleep(1.5)
+                        show_plot=False 
                     elif choice4=="1": #Area
                         print(f"\n{selected_shape.area()}\n")
                     elif choice4=="2": #Circumference
                         print(f"\n{selected_shape.perimeter}\n")
                     elif choice4=="3": #Move it
-                        x,y=input("Please enter the distances of which you want to move your circle in the X and Y directions").split()
+                        x,y=input("Please enter the distances of which you want to move your circle in the X and Y directions\n\n").split()
                         x=float(x) if str(float(x))==x or str(int(x))==x else x
                         y=float(y) if str(float(y))==y or str(int(y))==y else y
                         try:
                             selected_shape.move(x,y)
                         except (ValueError,TypeError) as err:
                             print(err)
+                            time.sleep(1.5)
+                            show_plot=False 
 
                     elif choice4=="4": #Move to a certain point
                         os.system('cls' if os.name == 'nt' else 'clear')
-                        x,y=input("Please enter the coordinates of the point you want to move your cirle to").split()
+                        x,y=input("Please enter the coordinates of the point you want to move your cirle to\n\n").split()
                         x=float(x) if str(float(x))==x or str(int(x))==x else x
                         y=float(y) if str(float(y))==y or str(int(y))==y else y
                         try:
                             selected_shape.move_to(x,y)
                         except (ValueError,TypeError) as err:
                             print(err)
+                            time.sleep(1.5)
+                            show_plot=False 
 
                     elif choice4=="5": #Scale it
                         os.system('cls' if os.name == 'nt' else 'clear')
@@ -146,6 +180,8 @@ while True:
                             selected_shape.scale(scaling_value)
                         except (ValueError,TypeError) as err:
                             print(err)
+                            time.sleep(1.5)
+                            show_plot=False 
 
                     elif choice4=="6": #Change radius
                         os.system('cls' if os.name == 'nt' else 'clear')
@@ -155,8 +191,11 @@ while True:
                             selected_shape.change_radius(new_radius)
                         except (ValueError,TypeError) as err:
                             print(err)
+                            time.sleep(1.5)
+                            show_plot=False 
 
                     elif choice4=="7": ##Check if it contains a point
+                        show_plot=False 
                         os.system('cls' if os.name == 'nt' else 'clear')
                         x,y=input("Please enter the coordinates of the point ").split()
                         x=float(x) if str(float(x))==x or str(int(x))==x else x
@@ -164,10 +203,13 @@ while True:
                         try:
                             if selected_shape.contain(x,y) :
                                 print(f"The point ({x},{y}) is withing this circle")
+                               
                             else :
                                 print(f"The point ({x},{y}) is withing this circle")
+                                
                         except (ValueError,TypeError) as err:
                             print(err)
+                            time.sleep(1.5)
                     elif choice4=="8": #Remove it
                         circle_list.remove(circle_list[choice3])
                     
@@ -175,6 +217,8 @@ while True:
         elif choice2 =="B":
             if len(rectangle_list)==0:
                 print("There are no rectangles to be selected")
+                time.sleep(1.5)
+                show_plot=False 
             else:
                 print("Please select a rectangle by entering its number")
                 os.system('cls' if os.name == 'nt' else 'clear')
@@ -186,12 +230,16 @@ while True:
 
                 if choice3>=len(rectangle_list) or choice3<0:
                     print("Invalid index")
+                    time.sleep(1.5)
+                    show_plot=False 
                 else:
                     selected_shape=rectangle_list[choice3][1]
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     choice4=input(menu_select_rectangle)
                     if choice4 not in ["1","2","3","4","5","6","7","8","9","10"]:
                         os.system('cls' if os.name == 'nt' else 'clear')
                         print("Please enter a valid choice")
+                        show_plot=False 
                     elif choice4=="1": #Area
                         os.system('cls' if os.name == 'nt' else 'clear')
                         print(f"\n{selected_shape.area()}\n")
@@ -207,6 +255,8 @@ while True:
                             rectangle_list[choice3][1]=selected_shape.move(x,y)
                         except (ValueError,TypeError) as err:
                             print(err)
+                            time.sleep(1.5)
+                            show_plot=False 
 
                     elif choice4=="4": #Move to a certain point
                         os.system('cls' if os.name == 'nt' else 'clear')
@@ -217,6 +267,8 @@ while True:
                             rectangle_list[choice3][1]=selected_shape.move_to(x,y)
                         except (ValueError,TypeError) as err:
                             print(err)
+                            time.sleep(1.5)
+                            show_plot=False 
 
 
                     elif choice4=="5": #Scale it
@@ -227,6 +279,8 @@ while True:
                             rectangle_list[choice3][1]=selected_shape.scale(scaling_value)
                         except (ValueError,TypeError) as err:
                             print(err)
+                            time.sleep(1.5)
+                            show_plot=False 
 
                     elif choice4=="6": #Change dimensions
                         os.system('cls' if os.name == 'nt' else 'clear')
@@ -237,6 +291,8 @@ while True:
                             rectangle_list[choice3][1]=selected_shape.change_size(new_x,new_y)
                         except (ValueError,TypeError) as err:
                             print(err)
+                            time.sleep(1.5)
+                            show_plot=False 
 
                     elif choice4=="7": #rotate
                         os.system('cls' if os.name == 'nt' else 'clear')
@@ -247,6 +303,8 @@ while True:
                             rectangle_list[choice3][1]=selected_shape.rotate(angle)
                         except (ValueError,TypeError) as err:
                             print(err)
+                            time.sleep(1.5)
+                            show_plot=False 
 
                     elif choice4=="8": #Make Horizontal
                         rectangle_list[choice3][1]=selected_shape.make_horizontal()
@@ -261,7 +319,9 @@ while True:
 
             if len(square_list)==0:
                 os.system('cls' if os.name == 'nt' else 'clear')
-                print("There are no rectangles to be selected")
+                print("There are no squares to be selected")
+                time.sleep(1.5)
+                show_plot=False 
             else:
                 os.system('cls' if os.name == 'nt' else 'clear')
                 print("Please select a square by entering its number")
@@ -273,11 +333,15 @@ while True:
 
                 if choice3>=len(square_list) or choice3<0:
                     print("Invalid index")
+                    time.sleep(1.5)
+                    show_plot=False 
                 else:
                     selected_shape=square_list[choice3][1]
                     choice4=input(menu_select_square)
                     if choice4 not in ["1","2","3","4","5","6","7","8","9"]:
                         print("Please enter a valid choice")
+                        time.sleep(1.5)
+                        show_plot=False 
                     elif choice4=="1": #Area
                         os.system('cls' if os.name == 'nt' else 'clear')
                         print(f"\n{selected_shape.area()}\n")
@@ -293,6 +357,8 @@ while True:
                             selected_shape.move(x,y)
                         except (ValueError,TypeError) as err:
                             print(err)
+                            time.sleep(1.5)
+                            show_plot=False 
 
                     elif choice4=="4": #Move to a certain point
                         os.system('cls' if os.name == 'nt' else 'clear')
@@ -303,6 +369,8 @@ while True:
                             selected_shape.move_to(x,y)
                         except (ValueError,TypeError) as err:
                             print(err)
+                            time.sleep(1.5)
+                            show_plot=False 
 
 
                     elif choice4=="5": #Scale it
@@ -313,6 +381,8 @@ while True:
                             selected_shape.scale(scaling_value)
                         except (ValueError,TypeError) as err:
                             print(err)
+                            time.sleep(1.5)
+                            show_plot=False 
 
                     elif choice4=="6": #Change dimensions
                         os.system('cls' if os.name == 'nt' else 'clear')
@@ -322,6 +392,8 @@ while True:
                             selected_shape.change_size(new_x,new_x)
                         except (ValueError,TypeError) as err:
                             print(err)
+                            time.sleep(1.5)
+                            show_plot=False 
 
                     elif choice4=="7": #rotate
                         os.system('cls' if os.name == 'nt' else 'clear')
@@ -332,48 +404,44 @@ while True:
                             selected_shape.rotate(angle) 
                         except (ValueError,TypeError) as err:
                             print(err)
+                            time.sleep(1.5)
+                            show_plot=False 
 
                     elif choice4=="8": #Make Horizontal
                         selected_shape.make_horizontal()
 
                     elif choice4=="9": #Remove it
                         square_list.remove(square_list[choice3])
-        elif choice2 =="M":
+
+        elif choice2 =="M": # Back to the Main Menu
+            show_plot=False 
             continue
 
-    elif choice1=="3":
+    elif choice1=="3": # Quit
         break
 
-    plt.ion()
-     
-    for circle in circle_list:
-        circle[1].plot()
-
-    for rectangle in rectangle_list:
-        (rectangle[1]).plot()
-
-    for square in square_list:
-        square.plot()
     
-    plt.axis('equal')
-    plt.show()
+
+    if show_plot:
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Please close the plot figure to continue")
+        for circle in circle_list:
+            circle[1].plot()
+
+        for rectangle in rectangle_list:
+            rectangle[1].plot()
+
+        for square in square_list:
+            square[1].plot()
+        
+
+        plt.gca().set_aspect('equal', adjustable='box')
+        
+        time.sleep(1)
+        plt.show()
 
 
 
-rectangle_list=[]
-square_list=[]
-circle_list=[]
 
 
-shape1=Shape(1,2)
-rec1=Rectangle(0.5,0.5,0.5,0.2)
-rec2= Rectangle(0.5,0.5,0.5,0.2,20)
-
-rec2.rotate(45)
-rec2.rotate(25)
-
-'''rec1.plot()
-rec2.plot()
-plt.show()
-
-print(rec1)'''
